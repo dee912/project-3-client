dbc062ff8b0ec6d8bbcb3a916068109cea1b035a
dee912 <devantespaine@gmail.com>
2021-05-22 17:36:46 +0100

    m8 profile layout

--- src/components/m8s/Profile.js
+++ src/components/m8s/Profile.js
@@ -28,7 +28,7 @@ export default function M8Show() {
           <div className="container">
             <div className="columns">
 
-              <div className="column is-4 is-offset-0">
+              <div className="column is-5 is-offset-0">
                 <div className="card">
                   <div className="card-image">
                     <div className="card-content">
@@ -63,23 +63,165 @@ export default function M8Show() {
                 </div>
                 <br/>
                 <div className="media-content">
-                  <p className="title is-5">Throw a pl8 Highscore</p>
+                  <p className="title is-4">Throw a pl8 Highscore</p>
                 </div>
                 <br/>
                 <div className="media-content">
                   <p className="title is-6">{m8.highScore}</p>
                 </div>
                 <br/>
+                
                 <div className="media-content">
                   <p className="title is-4">{m8.username}&apos;s m8&apos;s</p>
                 </div>
                 <br/>
-                <div className="media-content">
-                  <p className="title is-6"></p>
+  
+                <div className="columns">
+                  <div className="column is-4 is-offset-0">
+                    <div className="card">
+                      <div className="card-image">
+                        <div className="card-content">
+                          <div className="media">
+                            <div className="media-left">
+                            </div>
+                            <div className="media-content">
+                              <p className="title is-4">Chris</p>
+                            </div>
+                          </div>
+                        </div>
+                        <figure className="image is-4by3">
+                          <img src="https://static.boredpanda.com/blog/wp-content/uploads/2020/10/rate-my-plate-facebook-group-5f7f126f135cc-png__700.jpg" alt={m8.username}/>
+                        </figure>
+                      </div>
+                    </div>                 
+                  </div>
+                  <div className="column is-4 is-offset-0">
+                    <div className="card">
+                      <div className="card-image">
+                        <div className="card-content">
+                          <div className="media">
+                            <div className="media-left">
+                            </div>
+                            <div className="media-content">
+                              <p className="title is-4">Dom</p>
+                            </div>
+                          </div>
+                        </div>
+                        <figure className="image is-4by3">
+                          <img src="https://i2-prod.mirror.co.uk/incoming/article14433668.ece/ALTERNATES/s615b/0_Rate-My-Plate.jpg" alt={m8.username}/>
+                        </figure>
+                      </div>
+                    </div>                 
+                  </div>
+                  <div className="column is-4 is-offset-0">
+                    <div className="card">
+                      <div className="card-image">
+                        <div className="card-content">
+                          <div className="media">
+                            <div className="media-left">
+                            </div>
+                            <div className="media-content">
+                              <p className="title is-4">Nick</p>
+                            </div>
+                          </div>
+                        </div>
+                        <figure className="image is-4by3">
+                          <img src="https://static.boredpanda.com/blog/wp-content/uploads/2020/10/rate-my-plate-facebook-group-5f7f1377e3006-png__700.jpg" alt={m8.username}/>
+                        </figure>
+                      </div>
+                    </div>                 
+                  </div>
+                  
                 </div>
               </div>
               
             </div>
+            <div className="media-content">
+              <p className="title is-4">This m8&apos;s pl8&apos;s</p>
+            </div>
+            <br/>
+            <div className="columns">
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div className="media-content">
+              <p className="title is-4">This m8&apos;s faviourite pl8&apos;s</p>
+            </div>
+            <br/>
+            <div className="columns">
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-3 is-offset-0">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={m8.avatar} alt={m8.username}/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       }
