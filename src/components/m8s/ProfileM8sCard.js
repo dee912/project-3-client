3aa59e9927162b025b4745e92545c2db3fd1cca7
dee912 <devantespaine@gmail.com>
2021-05-24 14:42:43 +0100

    m8 friends list start

--- src/components/m8s/ProfileM8sCard.js
+++ src/components/m8s/ProfileM8sCard.js
@@ -2,29 +2,30 @@ import { Link } from 'react-router-dom'
 
 export default function ProfileM8sCard({ _id, username, avatar }) {
   return (
-
-    <Link to={`/m8/${_id}`}>
-      <div className="columns">
-        <div className="column is-4 is-offset-0">
-          <div className="card">
-            <div className="card-image">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">{username}</p>
+    
+    <section className="friends">
+      <Link to={`/m8/${_id}`}>
+        <div className="columns">
+          <div className="column is-4 is-offset-0">
+            <div className="card">
+              <div className="card-image">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">{username}</p>
+                    </div>
                   </div>
                 </div>
+                <figure className="image is-4by3">
+                  <img src={avatar} alt={username}/>
+                </figure>
               </div>
-              <figure className="image is-4by3">
-                <img src={avatar} alt={username}/>
-              </figure>
-            </div>
-          </div>                 
+            </div>                 
+          </div>
         </div>
-      </div>
-    </Link>
-    
+      </Link>
+    </section>
   )
 }
