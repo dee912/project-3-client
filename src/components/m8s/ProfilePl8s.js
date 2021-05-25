890bc85494477cf6a0f1cabe18af51c99507ba8e
dee912 <devantespaine@gmail.com>
2021-05-25 20:29:08 +0100

    show own plates

--- src/components/m8s/ProfilePl8s.js
+++ src/components/m8s/ProfilePl8s.js
@@ -1,48 +1,59 @@
-export default function ProfilePl8s({ avatar, username }) {
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+import { getAllPl8s, m8Profile } from '../../lib/api'
+
+export default function ProfilePl8s({ username }) {
+  const { m8Id } = useParams()
+  const [pl8, setPl8] = React.useState(null)
+  const [m8, setM8] = React.useState(null)
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await getAllPl8s()
+        setPl8(res.data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getData()
+  }, [])
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await m8Profile(m8Id)
+        setM8(res.data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getData()
+  }, [m8Id])
+
+  const ownPl8s = (pl8 && m8 && pl8.filter(pl8s => pl8s.m8 === m8._id))
+  
   return (
-    <div>
+    <section>
       <div className="media-content">
-        <p className="title is-4">This m8&apos;s pl8&apos;s</p>
+        <p className="title is-4">{username}&apos;s m8s</p>
       </div>
       <br/>
-      <div className="columns">
-        <div className="column is-3 is-offset-0">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
-                <img src={avatar} alt={username}/>
-              </figure>
+      <div className="containerScroll">
+        {ownPl8s && (
+          ownPl8s.map(ownPl8s => (
+            <div key={ownPl8s._id} >
+              <div  className="cardScroll">
+                <Link to={`/pl8s/${ownPl8s._id}` } >
+                  <p className="title is-4">{ownPl8s.name}</p>
+                  <img className="friends" src={ownPl8s.image} alt={ownPl8s.username}/>
+                </Link>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="column is-3 is-offset-0">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
-                <img src={avatar} alt={username}/>
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div className="column is-3 is-offset-0">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
-                <img src={avatar} alt={username}/>
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div className="column is-3 is-offset-0">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
-                <img src={avatar} alt={username}/>
-              </figure>
-            </div>
-          </div>
-        </div>
+          ))
+        )}
       </div>
-    </div>
+    </section>
   )
 }
