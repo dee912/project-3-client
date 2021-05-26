d32439a7bde36441bdec5d8f6658fbf64ddda4c8
dee912 <devantespaine@gmail.com>
2021-05-26 15:56:55 +0100

    search bar and index css

--- src/components/pl8s/PL8Card.js
+++ src/components/pl8s/PL8Card.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function PL8Card({ name, image, _id }) {
   return (
     <Link to={`/pl8s/${_id}`}>
-      <div className="list">
+      <div className="cardScroll">
         <h5 className="title">{name}</h5>
         <img className="friends" src={image} alt="name"/>
       </div>  
