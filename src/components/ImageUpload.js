5a6f7bcdc2cb2620d63b2421cc2d52990b5720e0
dee912 <devantespaine@gmail.com>
2021-05-26 16:07:18 +0100

    search bar and index css

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -29,7 +29,7 @@ function ImageUpload({ onUpload }) {
   return (
     <>
       {image && <img src={image} alt="uploaded profile"/>}
-      {!image && <button onClick={handleUpload} type="button" className="button is-fullwidth is-info">Upload Image</button>}
+      {!image ? <button onClick={handleUpload} type="button" className="button is-fullwidth is-info">Upload Image</button> : <button onClick={handleUpload} type="button" className="button is-fullwidth is-info">Upload Image</button>}
     </>
   )
 }
