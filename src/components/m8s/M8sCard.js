a039229c4b60fb9ec58d05935dae46b5360fa534
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-05-28 16:34:37 +0100

    deleted m8s in the m8s list

--- src/components/m8s/M8sCard.js
+++ src/components/m8s/M8sCard.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import { m8Profile, removeM8 } from '../../lib/api'
 import React from 'react'
 
-export default function M8sCard({ m8Id, setNewM8 }) {
+export default function M8sCard({ m8Id, setNewM8, setActiveM8s }) {
   const [m8, setM8] = React.useState(null)
 
   React.useEffect(() => {
@@ -21,20 +21,26 @@ export default function M8sCard({ m8Id, setNewM8 }) {
     try {
       const { data } = await removeM8(m8._id)
       setNewM8(data)
+      setActiveM8s(false)
     } catch (error) {
       console.log(error)
     }
   }
   return (
     <>
-      {m8 && <div className="cardScroll">
-        <div className='m8Remover'>
-          <button className="removeM8" onClick={removingM8}>X</button>
-        </div>
-        <Link to={`/m8/${m8._id}`} >
-          <p className="title is-4">{m8.username}</p>
-          <img className="friends" src={m8.avatar} alt={m8.username} />
-        </Link>
+      {m8 && <div>
+        {m8.deleted === false && <div className="cardScroll">
+          {setActiveM8s(true)}
+          <div>
+            <div className='m8Remover'>
+              <button className="removeM8" onClick={removingM8}>X</button>
+            </div>
+            <Link to={`/m8/${m8._id}`} >
+              <p className="title is-4">{m8.username}</p>
+              <img className="friends" src={m8.avatar} alt={m8.username} />
+            </Link>
+          </div>
+        </div>}
       </div>}
     </>
 
